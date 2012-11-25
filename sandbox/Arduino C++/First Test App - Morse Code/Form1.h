71e450ed4c70d20e33e7532a333aa312fbd02269
smocny <sebastian.mocny@gmail.com>
2012-11-24 21:27:29 -0500

    This is mike's work. Messing around, looking at old code

---
+++ sandbox/Arduino C++/First Test App - Morse Code/Form1.h
@@ -0,0 +1,92 @@
+#pragma once
+
+namespace FirstTestAppMorseCode {
+
+	using namespace System;
+	using namespace System::ComponentModel;
+	using namespace System::Collections;
+	using namespace System::Windows::Forms;
+	using namespace System::Data;
+	using namespace System::Drawing;
+
+	/// <summary>
+	/// Summary for Form1
+	/// </summary>
+	public ref class Form1 : public System::Windows::Forms::Form
+	{
+	public:
+		Form1(void)
+		{
+			InitializeComponent();
+			//
+			//TODO: Add the constructor code here
+			//
+		}
+
+	protected:
+		/// <summary>
+		/// Clean up any resources being used.
+		/// </summary>
+		~Form1()
+		{
+			if (components)
+			{
+				delete components;
+			}
+		}
+	private: System::IO::Ports::SerialPort^  Arduino;
+	protected: 
+	private: System::Windows::Forms::Button^  button1;
+	private: System::ComponentModel::IContainer^  components;
+
+	private:
+		/// <summary>
+		/// Required designer variable.
+		/// </summary>
+
+
+#pragma region Windows Form Designer generated code
+		/// <summary>
+		/// Required method for Designer support - do not modify
+		/// the contents of this method with the code editor.
+		/// </summary>
+		void InitializeComponent(void)
+		{
+			this->components = (gcnew System::ComponentModel::Container());
+			this->Arduino = (gcnew System::IO::Ports::SerialPort(this->components));
+			this->button1 = (gcnew System::Windows::Forms::Button());
+			this->SuspendLayout();
+			// 
+			// button1
+			// 
+			this->button1->Location = System::Drawing::Point(37, 96);
+			this->button1->Name = L"button1";
+			this->button1->Size = System::Drawing::Size(75, 23);
+			this->button1->TabIndex = 0;
+			this->button1->Text = L"button1";
+			this->button1->UseVisualStyleBackColor = true;
+			this->button1->Click += gcnew System::EventHandler(this, &Form1::button1_Click);
+			// 
+			// Form1
+			// 
+			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
+			this->ClientSize = System::Drawing::Size(284, 262);
+			this->Controls->Add(this->button1);
+			this->Name = L"Form1";
+			this->Text = L"Form1";
+			this->ResumeLayout(false);
+
+		}
+#pragma endregion
+	private: System::Void button1_Click(System::Object^  sender, System::EventArgs^  e) {
+				this->Arduino->PortName = "COM5";  // Replace with your COM port!
+      this->Arduino->Open();
+      this->Arduino->Write( "7" );  // In the future,  you'll expand on this
+                                        // to write your custom data to the board
+      this->Arduino->Close();
+
+			 }
+	};
+}
+
---
+++ sandbox/Arduino C++/First Test App - Morse Code/ReadMe.txt
@@ -0,0 +1,40 @@
+========================================================================
+    APPLICATION : First Test App - Morse Code Project Overview
+========================================================================
+
+AppWizard has created this First Test App - Morse Code Application for you.  
+
+This file contains a summary of what you will find in each of the files that
+make up your First Test App - Morse Code application.
+
+First Test App - Morse Code.vcxproj
+    This is the main project file for VC++ projects generated using an Application Wizard. 
+    It contains information about the version of Visual C++ that generated the file, and 
+    information about the platforms, configurations, and project features selected with the
+    Application Wizard.
+
+First Test App - Morse Code.vcxproj.filters
+    This is the filters file for VC++ projects generated using an Application Wizard. 
+    It contains information about the association between the files in your project 
+    and the filters. This association is used in the IDE to show grouping of files with
+    similar extensions under a specific node (for e.g. ".cpp" files are associated with the
+    "Source Files" filter).
+
+First Test App - Morse Code.cpp
+    This is the main application source file.
+    Contains the code to display the form.
+
+Form1.h
+    Contains the implementation of your form class and InitializeComponent() function.
+
+AssemblyInfo.cpp
+    Contains custom attributes for modifying assembly metadata.
+
+/////////////////////////////////////////////////////////////////////////////
+Other standard files:
+
+StdAfx.h, StdAfx.cpp
+    These files are used to build a precompiled header (PCH) file
+    named First Test App - Morse Code.pch and a precompiled types file named StdAfx.obj.
+
+/////////////////////////////////////////////////////////////////////////////
